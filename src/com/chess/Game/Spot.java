2d7932b16614cb0c49fbe85b0eede1e8862d202f
usernamenna <56196315+usernamenna@users.noreply.github.com>
2020-12-22 14:38:37 +0200

    Update Spot.java

--- src/com/chess/Game/Spot.java
+++ src/com/chess/Game/Spot.java
@@ -1,4 +1,42 @@
 package com.chess.Game;
 
+import com.chess.Pieces.Piece;
+
 public class Spot {
+    private final int x;
+    private final int y;
+    private boolean empty;
+    private Piece piece;
+
+    public Spot(final int x, final int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX()
+    {
+        return x;
+    }
+    public int getY()
+    {
+        return y;
+    }
+    public boolean isEmpty()
+    {
+        return empty;
+    }
+    public void insertPiece(Piece piece)
+    {
+        empty = false;
+        this.piece = piece;
+    }
+    public void removePiece()
+    {
+        empty = true;
+        piece = null;
+    }
+    public Piece getPiece()
+    {
+        return piece;
+    }
 }
